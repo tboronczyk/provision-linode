19b89be97b7997839f15f29d44212ef98fd3d96e
Timothy Boronczyk <tboronczyk@gmail.com>
2018-11-20 05:12:59 +0000

    separate web and mail server setup

--- ansible/files/accounts.sqlite.sql
+++ ansible/files/accounts.sqlite.sql
@@ -9,15 +9,15 @@ CREATE TABLE users (
   password VARCHAR(128) NOT NULL,
   active CHAR(1) DEFAULT 'Y' NOT NULL
 );
-INSERT INTO users VALUES ('{{myuser}}', '{{mydomain}}', '{SHA512-CRYPT}\$6\$GU6NOMcXSdePd8tr\$kSi3g/SGdZuUcFETYvQKXTH2J125cIvJkyv5XWVkX/0UxgZSk6s61Zc9B7pzyZ9j8zCj8as3TKId.xepI1625/', 'Y');
+INSERT INTO users VALUES ('{{myusername}}', '{{mydomain}}', '{SHA512-CRYPT}\$6\$GU6NOMcXSdePd8tr\$kSi3g/SGdZuUcFETYvQKXTH2J125cIvJkyv5XWVkX/0UxgZSk6s61Zc9B7pzyZ9j8zCj8as3TKId.xepI1625/', 'Y');
 
 CREATE TABLE aliases (
   source VARCHAR(128) NOT NULL,
   destination VARCHAR(128) NOT NULL
 );
-INSERT INTO aliases VALUES ('abuse@{{mydomain}}', '{{myuser}}@{{mydomain}}');
-INSERT INTO aliases VALUES ('security@{{mydomain}}', '{{myuser}}@{{mydomain}}');
-INSERT INTO aliases VALUES ('postmaster@{{mydomain}}', '{{myuser}}@{{mydomain}}');
-INSERT INTO aliases VALUES ('hostmaster@{{mydomain}}', '{{myuser}}@{{mydomain}}');
-INSERT INTO aliases VALUES ('webmaster@{{mydomain}}', '{{myuser}}@{{mydomain}}');
+INSERT INTO aliases VALUES ('abuse@{{mydomain}}', '{{myusername}}@{{mydomain}}');
+INSERT INTO aliases VALUES ('security@{{mydomain}}', '{{myusername}}@{{mydomain}}');
+INSERT INTO aliases VALUES ('postmaster@{{mydomain}}', '{{myusername}}@{{mydomain}}');
+INSERT INTO aliases VALUES ('hostmaster@{{mydomain}}', '{{myusername}}@{{mydomain}}');
+INSERT INTO aliases VALUES ('webmaster@{{mydomain}}', '{{myusername}}@{{mydomain}}');
 
