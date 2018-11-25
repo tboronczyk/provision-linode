d63db90fb575c8b0eb52c5de78c5d7da589e3d9b
Timothy Boronczyk <tboronczyk@gmail.com>
2018-11-25 21:40:20 +0000

    safeguard for re-apply of tainted resource

--- terraform/modules/setup/main.tf
+++ terraform/modules/setup/main.tf
@@ -9,14 +9,14 @@ resource "null_resource" "setup" {
 
         inline = [
             # install Python dependency for Ansible
-            "apt-get -y install python",
+            "which python || apt-get -y install python",
 
             # non-root user account
-            "useradd -m -s /bin/bash ${var.user}",
-            "usermod -a -G sudo ${var.user}",
-            "echo '${var.user} ALL = NOPASSWD : ALL' > /etc/sudoers.d/${var.user}",
-            "mkdir /home/${var.user}/.ssh",
-            "echo '${file(var.ssh_pubkey)}' > /home/${var.user}/.ssh/authorized_keys",
+            "id -u ${var.user} || useradd -m -s /bin/bash ${var.user}",
+            "groups ${var.user} | grep -q sudo || usermod -a -G sudo ${var.user}",
+            "[ -f /etc/sudoers.d/${var.user} ] || echo '${var.user} ALL = NOPASSWD : ALL' > /etc/sudoers.d/${var.user}",
+            "[ -d /home/${var.user}/.ssh ] || mkdir /home/${var.user}/.ssh",
+            "[ -f /home/${var.user}/.ssh/authorized_keys ] || echo '${file(var.ssh_pubkey)}' > /home/${var.user}/.ssh/authorized_keys",
             "chown -R tboronczyk:tboronczyk /home/${var.user}/.ssh",
             "chmod 700 /home/${var.user}/.ssh",
             "chmod 600 /home/${var.user}/.ssh/authorized_keys"
